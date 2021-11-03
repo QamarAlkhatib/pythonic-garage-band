569ddbb0f01239641676fa6c72b9ec804add6a73
Qamar <qalkhatib0@gmail.com>
2021-11-03 18:27:12 +0200

    finished

---
+++ pythonic_garage_band/band.py
@@ -0,0 +1,90 @@
+from abc import ABC,abstractmethod
+       
+class Musician:
+
+    def __init__(self,name):
+        self.name = name
+
+    @abstractmethod
+    def __str__(self):
+        pass
+
+    @abstractmethod
+    def __repr__(self):
+        pass
+
+    @abstractmethod
+    def get_instrument(self):
+        pass
+
+    @abstractmethod
+    def play_solo(self):
+        pass
+
+
+class Band(Musician):
+
+    instances =[]
+    def __init__(self,name,members):
+        self.name = name
+        self.members = members
+        Band.instances.append(self)
+
+    def play_solos(self):
+        play_Solo_member=[]
+        for member in self.members:
+            play_Solo_member.append(member.play_solo())
+        return play_Solo_member
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+       return self.name
+
+    @classmethod
+    def to_list(cls):
+        return cls.instances
+
+class Guitarist(Musician):
+
+    def __str__(self):
+        return f"My name is {self.name} and I play guitar"
+
+    def __repr__(self):
+       return f"Guitarist instance. Name = {self.name}"
+
+    def get_instrument(self):
+        return 'guitar'
+
+    def play_solo(self):
+        return 'face melting guitar solo'
+
+class Bassist(Musician):
+
+    def __str__(self):
+        return f"My name is {self.name} and I play bass"
+
+    def __repr__(self):
+       return f"Bassist instance. Name = {self.name}"
+
+    def get_instrument(self):
+        return 'bass'
+
+    def play_solo(self):
+        return 'bom bom buh bom'
+
+
+class Drummer(Musician):
+
+    def __str__(self):
+        return f"My name is {self.name} and I play drums"
+
+    def __repr__(self):
+       return f"Drummer instance. Name = {self.name}"
+
+    def get_instrument(self):
+        return 'drums'
+
+    def play_solo(self):
+        return 'rattle boom crash'
